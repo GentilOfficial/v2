861454bcd422eb68a9ed46f516d331a6fa91fd1d
GentilOfficial <federicogentili05@gmail.com>
2023-09-03 23:39:23 +0200

    fixing Footer links animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,47 @@
 import React from "react";
 import { Link, Button } from "@nextui-org/react";
-import { useInView } from "framer-motion";
+import { useInView, useAnimation, motion } from "framer-motion";
 import { FaInstagram, FaGithub, FaTelegramPlane } from "react-icons/fa";
 import { SiteConfig } from "../data/links";
 
 export default function Footer(x) {
     const mail = React.useRef(null);
-    const linkInstagram = React.useRef(null);
-    const linkGithub = React.useRef(null);
-    const linkTelegram = React.useRef(null);
+    const links = React.useRef(null);
     const credits = React.useRef(null);
 
     const mailIsInView = useInView(mail, { once: true });
-    const linkInstagramIsInView = useInView(linkInstagram, { once: true });
-    const linkGithubIsInView = useInView(linkGithub, { once: true });
-    const linkTelegramIsInView = useInView(linkTelegram, { once: true });
+    const linksAreInView = useInView(links, { once: true });
     const creditsAreInView = useInView(credits, { once: true });
+
+    const linkContainerAnimation = useAnimation();
+
+    const linkContainer = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2,
+            },
+        },
+    };
+
+    const linkAnimation = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1,
+        },
+    };
+
+    React.useEffect(() => {
+        if (linksAreInView) {
+            linkContainerAnimation.start("visible");
+        } else {
+            linkContainerAnimation.start("hidden");
+        }
+    });
 
     return (
         <footer className="max-w-screen-lg mx-auto p-5 mt-5">
@@ -36,88 +62,70 @@ export default function Footer(x) {
                 >
                     {SiteConfig.email}
                 </Link>
-                <section className="grid grid-cols-2 sm:grid-cols-3 gap-4 items-center justify-center">
-                    <Button
-                        style={{
-                            transform: linkInstagramIsInView
-                                ? "none"
-                                : "translateY(20px)",
-                            opacity: linkInstagramIsInView ? 1 : 0,
-                            transitionProperty: "opacity, transform",
-                            transitionDuration: "0.9s",
-                            ease: "cubic-bezier(1.01, 0.2, 0.71, 0)",
-                        }}
-                        ref={linkInstagram}
-                        size="sm"
-                        as={Link}
-                        showAnchorIcon
-                        href={SiteConfig.links.instagram}
-                        isExternal
-                        className="bg-white/5 dark:bg-default-400/10 backdrop-blur shadow border dark:border-none"
+                <motion.section
+                    ref={links}
+                    variants={linkContainer}
+                    initial="hidden"
+                    animate={linkContainerAnimation}
+                    className="grid grid-cols-2 sm:grid-cols-3 gap-4 items-center justify-center"
+                >
+                    <motion.div variants={linkAnimation}>
+                        <Button
+                            size="sm"
+                            as={Link}
+                            showAnchorIcon
+                            href={SiteConfig.links.instagram}
+                            isExternal
+                            className="bg-white/5 dark:bg-default-400/10 backdrop-blur shadow border dark:border-none w-full"
+                        >
+                            <FaInstagram />
+                            Instagram
+                        </Button>
+                    </motion.div>
+                    <motion.div variants={linkAnimation}>
+                        <Button
+                            size="sm"
+                            as={Link}
+                            showAnchorIcon
+                            href={SiteConfig.links.telegram}
+                            isExternal
+                            className="bg-white/5 dark:bg-default-400/10 backdrop-blur shadow border dark:border-none w-full"
+                        >
+                            <FaTelegramPlane />
+                            Telegram
+                        </Button>
+                    </motion.div>
+                    <motion.div
+                        variants={linkAnimation}
+                        className="col-span-2 sm:col-span-1"
                     >
-                        <FaInstagram />
-                        Instagram
-                    </Button>
-                    <Button
-                        style={{
-                            transform: linkTelegramIsInView
-                                ? "none"
-                                : "translateY(20px)",
-                            opacity: linkTelegramIsInView ? 1 : 0,
-                            transitionProperty: "opacity, transform",
-                            transitionDuration: "0.9s",
-                            transitionDelay: "0.2s",
-                            ease: "cubic-bezier(1.01, 0.2, 0.71, 0)",
-                        }}
-                        ref={linkTelegram}
-                        size="sm"
-                        as={Link}
-                        showAnchorIcon
-                        href={SiteConfig.links.telegram}
-                        isExternal
-                        className="bg-white/5 dark:bg-default-400/10 backdrop-blur shadow border dark:border-none"
-                    >
-                        <FaTelegramPlane />
-                        Telegram
-                    </Button>
-                    <Button
-                        style={{
-                            transform: linkGithubIsInView
-                                ? "none"
-                                : "translateY(20px)",
-                            opacity: linkGithubIsInView ? 1 : 0,
-                            transitionProperty: "opacity, transform",
-                            transitionDuration: "0.9s",
-                            transitionDelay: "0.4s",
-                            ease: "cubic-bezier(1.01, 0.2, 0.71, 0)",
-                        }}
-                        ref={linkGithub}
-                        size="sm"
-                        as={Link}
-                        showAnchorIcon
-                        href={SiteConfig.links.github}
-                        isExternal
-                        className="bg-white/5 dark:bg-default-400/10 backdrop-blur shadow border dark:border-none col-span-2 sm:col-span-1"
-                    >
-                        <FaGithub />
-                        GitHub
-                    </Button>
-                </section>
-                <p
+                        <Button
+                            size="sm"
+                            as={Link}
+                            showAnchorIcon
+                            href={SiteConfig.links.github}
+                            isExternal
+                            className="bg-white/5 dark:bg-default-400/10 backdrop-blur shadow border dark:border-none w-full"
+                        >
+                            <FaGithub />
+                            GitHub
+                        </Button>
+                    </motion.div>
+                </motion.section>
+                <section
+                    ref={credits}
+                    className="w-44"
                     style={{
                         opacity: creditsAreInView ? 1 : 0,
                         transitionProperty: "opacity",
                         transitionDuration: "0.9s",
-                        transitionDelay: "0.6s",
                         ease: "cubic-bezier(1.01, 0.2, 0.71, 0)",
                     }}
-                    ref={credits}
-                    className="w-44"
                 >
                     {x.isIT
                         ? "Progettato e Realizzato da "
                         : "Designed & Built by "}
-                    <span
+                    <p
                         className="text-primary cursor-pointer"
                         onClick={() => {
                             x.toggleEasterEgg(!x.isEasterEggPlaying);
@@ -127,8 +135,8 @@ export default function Footer(x) {
                         }}
                     >
                         Federico Gentili
-                    </span>
-                </p>
+                    </p>
+                </section>
             </div>
         </footer>
     );
