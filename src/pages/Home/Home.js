8d8bedd7692bfeaa33ee64f9b85da8ca8a1a31d2
GentilOfficial <federicogentili05@gmail.com>
2023-09-05 00:12:02 +0200

    making HomeDivider component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,32 +1,13 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { BsArrowDownShort } from "react-icons/bs";
 import HomeHeader from "./components/HomeHeader";
+import HomeDivider from "./components/HomeDivider";
 import HomeFeatureSection from "./components/HomeFeatureSection";
 
-export default function HomeIndex(x) {
+export default function Home(x) {
     return (
         <div className="space-y-16">
             <HomeHeader isIT={x.isIT} />
-            <section className="flex items-center justify-center gap-2 text-primary">
-                <div className="w-full h-px bg-neutral-500/30"></div>
-                <motion.span
-                    animate={{
-                        y: [0, 5],
-                    }}
-                    transition={{
-                        y: {
-                            duration: 1,
-                            repeat: Infinity,
-                            ease: "easeInOut",
-                            repeatType: "reverse",
-                        },
-                    }}
-                >
-                    <BsArrowDownShort />
-                </motion.span>
-                <div className="w-full h-px bg-neutral-500/30"></div>
-            </section>
+            <HomeDivider />
             <HomeFeatureSection isIT={x.isIT} />
         </div>
     );
---
+++ src/pages/Home/components/HomeDivider.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { BsArrowDownShort } from "react-icons/bs";
+
+export default function HomeDivider(x) {
+    return (
+        <section className="flex items-center justify-center gap-2 text-primary">
+            <div className="w-full h-px bg-neutral-500/30"></div>
+            <motion.span
+                animate={{
+                    y: [0, 5],
+                }}
+                transition={{
+                    y: {
+                        duration: 1,
+                        repeat: Infinity,
+                        ease: "easeInOut",
+                        repeatType: "reverse",
+                    },
+                }}
+            >
+                <BsArrowDownShort />
+            </motion.span>
+            <div className="w-full h-px bg-neutral-500/30"></div>
+        </section>
+    );
+}
