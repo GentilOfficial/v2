8c13e82abfb8a3a7abcc405c195f474d81085f3a
GentilOfficial <federicogentili05@gmail.com>
2023-09-07 04:36:57 +0200

    fixing Skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -3,8 +3,8 @@ import { SkillsData } from "../data/SkillsData";
 
 export default function Skills(x) {
     return (
-        <section className="flex items-center justify-center py-16">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full">
+        <div className="flex items-center justify-center py-16">
+            <section className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full">
                 {SkillsData.map((item, index) => (
                     <SkillsBar
                         key={`${item}-${index}`}
@@ -12,7 +12,7 @@ export default function Skills(x) {
                         percent={item.percent}
                     />
                 ))}
-            </div>
-        </section>
+            </section>
+        </div>
     );
 }
