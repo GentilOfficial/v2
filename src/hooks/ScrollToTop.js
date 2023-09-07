f8818146e5170b287091d6065033ab465227f3da
GentilOfficial <federicogentili05@gmail.com>
2023-09-07 04:36:39 +0200

    adding title for sections

--- src/hooks/ScrollToTop.js
+++ src/hooks/ScrollToTop.js
@@ -1,12 +1,29 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { SiteConfig } from "../data/SiteConfig";
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
+    const isIT = pathname.slice(1).split("/")[0] === "it" ? true : false;
+
+    const indexOfNow = SiteConfig.navItems.findIndex(
+        isIT
+            ? (i) =>
+                  i.hrefIT.replaceAll("/", "") === pathname.replaceAll("/", "")
+            : (i) => i.href.replaceAll("/", "") === pathname.replaceAll("/", "")
+    );
+
     useEffect(() => {
+        document.title =
+            SiteConfig.navItems[indexOfNow].href === "/"
+                ? "Federico Gentili"
+                : isIT
+                ? "Federico Gentili - " +
+                  SiteConfig.navItems[indexOfNow].labelIT
+                : "Federico Gentili - " + SiteConfig.navItems[indexOfNow].label;
         window.scrollTo(0, 0);
-    }, [pathname]);
+    }, [pathname, indexOfNow, isIT]);
 
     return null;
 }
