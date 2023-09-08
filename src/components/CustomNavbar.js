1727b111ca22334587bfe4a6d47656f8b46a9b24
GentilOfficial <federicogentili05@gmail.com>
2023-09-08 02:28:01 +0200

    fixing navbar and removing animation

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -21,53 +21,9 @@ export default function CustomNavbar(x) {
 
     const location = useLocation().pathname;
 
-    const navContainer = {
-        hidden: { opacity: 1, scale: 0 },
-        visible: {
-            opacity: 1,
-            scale: 1,
-            transition: {
-                delayChildren: 0.3,
-                staggerChildren: 0.2,
-            },
-        },
-    };
-
-    const navItems = {
-        hidden: { y: -20, opacity: 0 },
-        visible: {
-            y: 0,
-            opacity: 1,
-            transition: {
-                delayChildren: 0.3,
-                staggerChildren: 0.2,
-            },
-        },
-    };
-
-    const navItemsChild = {
-        hidden: { y: -20, opacity: 0 },
-        visible: {
-            y: 0,
-            opacity: 1,
-        },
-    };
-
     return (
-        <Navbar
-            as={motion.nav}
-            variants={navContainer}
-            initial="hidden"
-            animate="visible"
-            onMenuOpenChange={setIsMenuOpen}
-            disableAnimation
-            isMenuOpen={isMenuOpen}
-        >
-            <NavbarBrand
-                as={motion.div}
-                variants={navItems}
-                className="select-none"
-            >
+        <Navbar onMenuOpenChange={setIsMenuOpen} isMenuOpen={isMenuOpen}>
+            <NavbarBrand className="select-none">
                 <LinkHref
                     href={SiteConfig.domain}
                     color="foreground"
@@ -86,17 +42,10 @@ export default function CustomNavbar(x) {
                 </LinkHref>
             </NavbarBrand>
 
-            <NavbarContent
-                as={motion.ul}
-                variants={navItems}
-                className="hidden sm:flex gap-4"
-                justify="center"
-            >
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 {SiteConfig.navItems.map((item, index) => (
                     <NavbarItem
                         key={`${item}-${index}`}
-                        as={motion.li}
-                        variants={navItemsChild}
                         isActive={
                             x.isIT
                                 ? location.replaceAll("/", "") ===
@@ -132,18 +81,14 @@ export default function CustomNavbar(x) {
                 ))}
             </NavbarContent>
 
-            <NavbarContent as={motion.ul} variants={navItems} justify="end">
-                <NavbarItem as={motion.li} variants={navItemsChild}>
+            <NavbarContent justify="end">
+                <NavbarItem>
                     <ThemeSwitcher />
                 </NavbarItem>
-                <NavbarItem as={motion.li} variants={navItemsChild}>
+                <NavbarItem>
                     <LanguageSelector isIT={x.isIT} />
                 </NavbarItem>
-                <NavbarItem
-                    as={motion.li}
-                    variants={navItemsChild}
-                    className="hidden sm:block"
-                >
+                <NavbarItem className="hidden sm:block">
                     <Button
                         as={LinkHref}
                         color="primary"
@@ -154,59 +99,51 @@ export default function CustomNavbar(x) {
                         {x.isIT ? "Curriculum" : "Resume"}
                     </Button>
                 </NavbarItem>
+                <Button
+                    as={NavbarMenuToggle}
+                    isIconOnly
+                    size="sm"
+                    variant="light"
+                    className="block sm:hidden"
+                />
             </NavbarContent>
 
-            <NavbarMenuToggle
-                as={motion.button}
-                variants={navItems}
-                className="sm:hidden"
-            />
-
-            <NavbarMenu>
-                <motion.div
-                    className="flex flex-col gap-6 items-center justify-center min-h-[calc(100vh-25rem)]"
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.4, ease: [0, 0.71, 0.2, 1.01] }}
-                >
-                    {SiteConfig.navItems.map((item, index) => (
-                        <NavbarMenuItem key={`${item}-${index}`}>
-                            <LinkHref
-                                as={Link}
-                                to={x.isIT ? item.hrefIT : item.href}
-                                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                                className={`rounded ${
-                                    index === 0 ? "mt-5" : ""
-                                } ${
-                                    x.isIT
-                                        ? location.replaceAll("/", "") ===
-                                          item.hrefIT.replaceAll("/", "")
-                                            ? "text-primary font-bold text-4xl"
-                                            : "text-foreground text-2xl"
-                                        : location.replaceAll("/", "") ===
-                                          item.href.replaceAll("/", "")
+            <NavbarMenu className="text-center gap-6">
+                {SiteConfig.navItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <LinkHref
+                            as={Link}
+                            to={x.isIT ? item.hrefIT : item.href}
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                            className={`rounded ${index === 0 ? "mt-5" : ""} ${
+                                x.isIT
+                                    ? location.replaceAll("/", "") ===
+                                      item.hrefIT.replaceAll("/", "")
                                         ? "text-primary font-bold text-4xl"
                                         : "text-foreground text-2xl"
-                                }`}
-                            >
-                                {x.isIT ? item.labelIT : item.label}
-                            </LinkHref>
-                        </NavbarMenuItem>
-                    ))}
-                    <NavbarItem className="w-full gap-4 flex flex-col items-center">
-                        <Divider className="w-3/5" />
-                        <Button
-                            as={LinkHref}
-                            size="lg"
-                            color="primary"
-                            href={x.isIT ? "/it/Curriculum.pdf" : "/Resume.pdf"}
-                            variant="flat"
-                            isExternal
+                                    : location.replaceAll("/", "") ===
+                                      item.href.replaceAll("/", "")
+                                    ? "text-primary font-bold text-4xl"
+                                    : "text-foreground text-2xl"
+                            }`}
                         >
-                            {x.isIT ? "Curriculum" : "Resume"}
-                        </Button>
-                    </NavbarItem>
-                </motion.div>
+                            {x.isIT ? item.labelIT : item.label}
+                        </LinkHref>
+                    </NavbarMenuItem>
+                ))}
+                <NavbarMenuItem className="w-full gap-6 flex flex-col items-center">
+                    <Divider className="w-3/5" />
+                    <Button
+                        as={LinkHref}
+                        size="lg"
+                        color="primary"
+                        href={x.isIT ? "/it/Curriculum.pdf" : "/Resume.pdf"}
+                        variant="flat"
+                        isExternal
+                    >
+                        {x.isIT ? "Curriculum" : "Resume"}
+                    </Button>
+                </NavbarMenuItem>
             </NavbarMenu>
         </Navbar>
     );
