95c6b5d94fbe9ee75c259ddec4ad35157e50319d
GentilOfficial <federicogentili05@gmail.com>
2023-09-10 21:22:04 +0200

    changing images

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,58 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Image } from "@nextui-org/react";
-import { motion } from "framer-motion";
-import Chilling from "../assets/images/chilling.webp";
+import NotFoundImage from "../assets/images/notFound.webp";
 
 export default function NotFound(x) {
     return (
-        <div className="flex items-center justify-center">
-            <section className="flex items-center py-16 mx-auto">
-                <div className="flex flex-col items-center justify-center text-center">
-                    <p className="text-sm font-medium text-primary">
-                        {x.isIT ? "Codice errore: 404" : "Error code: 404"}
-                    </p>
-                    <h1 className="mt-3 text-3xl font-semibold">
-                        {x.isIT ? "Pagina non trovata" : "Page not found"}
-                    </h1>
-                    <p className="mt-2 text-xl text-foreground-500 dark:text-foreground-400 font-caveat">
-                        {x.isIT
-                            ? "Mi spiace, la pagina che stai cercando non esiste oppure è stata spostata."
-                            : "Sorry, the page you are looking for doesn't exist or has been moved."}
-                    </p>
-
-                    <div className="flex items-center mt-4 gap-x-3">
-                        <Button
-                            as={Link}
-                            to={x.isIT ? "/it/" : "/"}
-                            variant="bordered"
-                            color="primary"
-                        >
-                            {x.isIT ? "Vai alla Home" : "Go Home"}
-                        </Button>
-                    </div>
-                    <motion.div
-                        className="flex justify-center"
-                        animate={{
-                            y: [10, -10],
-                        }}
-                        transition={{
-                            duration: 5,
-                            ease: "easeInOut",
-                            repeat: Infinity,
-                            repeatType: "reverse",
-                        }}
-                    >
-                        <Image
-                            isBlurred
-                            width={300}
-                            className="mx-auto"
-                            alt="Error me image"
-                            src={Chilling}
-                        />
-                    </motion.div>
-                </div>
-            </section>
-        </div>
+        <section className="flex flex-col items-center justify-center min-h-[calc(100vh-21rem)] sm:min-h-[calc(100vh-18rem)] text-center">
+            <p className="text-sm font-medium text-primary mt-6">
+                {x.isIT ? "Codice errore: 404" : "Error code: 404"}
+            </p>
+            <h1 className="mt-3 text-3xl font-semibold">
+                {x.isIT ? "Pagina non trovata" : "Page not found"}
+            </h1>
+            <p className="mt-2 text-xl text-foreground-500 dark:text-foreground-400 font-caveat">
+                {x.isIT
+                    ? "Mi spiace, la pagina che stai cercando non esiste oppure è stata spostata."
+                    : "Sorry, the page you are looking for doesn't exist or has been moved."}
+            </p>
+            <Button
+                as={Link}
+                to={x.isIT ? "/it/" : "/"}
+                variant="bordered"
+                color="primary"
+                className="mx-auto mt-5 mb-10"
+            >
+                {x.isIT ? "Vai alla Home" : "Go Home"}
+            </Button>
+            <Image
+                isBlurred
+                width={300}
+                className="mx-auto"
+                alt="Error me image"
+                src={NotFoundImage}
+            />
+        </section>
     );
 }
