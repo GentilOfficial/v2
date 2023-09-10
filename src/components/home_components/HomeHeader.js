d333018feb076f10ebb7438cbd668cf90a1a208c
GentilOfficial <federicogentili05@gmail.com>
2023-09-10 22:00:51 +0200

    editing home page

--- src/components/home_components/HomeHeader.js
+++ src/components/home_components/HomeHeader.js
@@ -1,111 +1,78 @@
 import { Image } from "@nextui-org/image";
 import { motion } from "framer-motion";
-import Fly from "../../assets/images/fly.webp";
-import Hello from "../../assets/images/hello.webp";
+import Welcome from "../../assets/images/welcome.webp";
 
 export default function HomeHeader(x) {
+    const headerContainer = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2,
+            },
+        },
+    };
+
+    const headerItem = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2,
+            },
+        },
+    };
+
     return (
-        <section className="py-16 relative flex min-h-[calc(100vh-12.7rem)] md:min-h-[calc(100vh-12rem)]">
+        <motion.section
+            variants={headerContainer}
+            initial="hidden"
+            animate="visible"
+            className="flex flex-col justify-center min-h-[calc(100vh-4rem)]"
+        >
+            <motion.h1
+                variants={headerItem}
+                className="font-bold text-center font-caveat select-none tracking-widest text-lg sm:text-xl md:text-2xl lg:text-3xl mt-6"
+            >
+                {x.isIT ? "Hey, io sono" : "Hey, I’m"}
+            </motion.h1>
+            <motion.h2
+                variants={headerItem}
+                className="font-bold text-center font-caveat text-primary mt-2 text-6xl sm:text-7xl md:text-8xl lg:text-9xl"
+            >
+                Federico Gentili
+            </motion.h2>
+            <motion.h3
+                variants={headerItem}
+                className="font-bold text-center font-caveat mt-1 mb-1 text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-foreground-500"
+            >
+                {x.isIT
+                    ? "Benvenuto nel mio spazio digitale"
+                    : "Welcome to my digital space"}
+            </motion.h3>
+            <motion.p
+                variants={headerItem}
+                className="text-center text-lg sm:text-xl md:text-2xl lg:text-3xl font-caveat mt-5 text-foreground-500"
+            >
+                {x.isIT
+                    ? "Sono uno sviluppatore web fullstack, proveniente da Ferrara, Italia."
+                    : "I'm an italian fullstack web developer, based near Ferrara, Italy."}
+            </motion.p>
             <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{
-                    type: "spring",
-                    stiffness: 260,
-                    damping: 20,
-                    delay: 2,
-                }}
-                className="absolute w-[calc(100%+20rem)] -translate-x-6 h-full top-20 left-0 bg-looper-pattern bg-center bg-no-repeat bg-cover md:bg-contain"
-            ></motion.div>
-            <section className="flex flex-col gap-6 items-center md:items-start md:justify-center w-full md:w-1/2 z-10">
-                <motion.div
-                    initial={{ y: 200, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{
-                        type: "spring",
-                        stiffness: 260,
-                        damping: 20,
-                        delay: 1,
-                    }}
-                    className="flex flex-col gap-1 items-center"
-                >
-                    <Image
-                        isBlurred
-                        width={200}
-                        height={200}
-                        className="md:hidden"
-                        alt="Hi image"
-                        src={Hello}
-                    />
-                    <h3 className="text-4xl lg:text-5xl font-bold uppercase text-center md:text-start mb-2 mt-1">
-                        {x.isIT ? "Hey, io sono " : "Hey, I’m "}
-                        <span className="text-primary block">
-                            FEDERICO GENTILI
-                        </span>
-                        {x.isIT ? " benvenuto nel mio " : " welcome to my "}
-                        <span className="text-primary inline min-[600px]:block md:inline">
-                            PORTFOLIO
-                        </span>
-                    </h3>
-                </motion.div>
-                <motion.p
-                    initial={{ y: 200, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{
-                        type: "spring",
-                        stiffness: 260,
-                        damping: 20,
-                        delay: 1.5,
-                    }}
-                    className="text-xl md:text-2xl text-default-500 font-caveat text-center md:text-start max-w-sm"
-                >
-                    {x.isIT
-                        ? "Sono uno sviluppatore web fullstack, proveniente da Ferrara, Italia."
-                        : "I'm an italian fullstack web developer, based near Ferrara, Italy."}
-                </motion.p>
-            </section>
-            <section className="hidden md:flex w-1/2 items-center">
-                <motion.div
-                    initial={{ scale: 0, opacity: 0 }}
-                    animate={{ scale: 1, opacity: 1 }}
-                    transition={{
-                        type: "spring",
-                        stiffness: 260,
-                        damping: 20,
-                        delay: 2,
-                    }}
-                >
-                    <motion.div
-                        animate={{
-                            y: [0, 20],
-                            rotate: [0, -5, 2, 0],
-                        }}
-                        transition={{
-                            delay: 2,
-                            y: {
-                                duration: 4,
-                                repeat: Infinity,
-                                ease: "easeInOut",
-                                repeatType: "reverse",
-                            },
-                            rotate: {
-                                duration: 8,
-                                repeat: Infinity,
-                                ease: "easeInOut",
-                                repeatType: "loop",
-                            },
-                        }}
-                    >
-                        <Image
-                            isBlurred
-                            alt="Fly image"
-                            width="100%"
-                            height="100%"
-                            src={Fly}
-                        />
-                    </motion.div>
-                </motion.div>
-            </section>
-        </section>
+                variants={headerItem}
+                className="flex items-center justify-center"
+            >
+                <Image
+                    isBlurred
+                    alt="Welcome"
+                    width={350}
+                    height={350}
+                    src={Welcome}
+                />
+            </motion.div>
+        </motion.section>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import HomeHeader from "../components/home_components/HomeHeader";
-import HomeDivider from "../components/home_components/HomeDivider";
 import HomeFeatureSection from "../components/home_components/HomeFeatureSection";
 
 export default function Home(x) {
     return (
         <div className="space-y-16">
             <HomeHeader isIT={x.isIT} />
-            <HomeDivider />
             <HomeFeatureSection isIT={x.isIT} />
         </div>
     );
