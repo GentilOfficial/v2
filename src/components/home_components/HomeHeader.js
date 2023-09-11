822cb418c08d764ccd43948037d411466a9af2bf
GentilOfficial <federicogentili05@gmail.com>
2023-09-12 01:13:13 +0200

    fixing home header

--- src/components/home_components/HomeHeader.js
+++ src/components/home_components/HomeHeader.js
@@ -35,19 +35,19 @@ export default function HomeHeader(x) {
         >
             <motion.h1
                 variants={headerItem}
-                className="font-bold text-center font-caveat tracking-widest text-lg sm:text-xl md:text-2xl lg:text-3xl mt-6"
+                className="drop-shadow-lg font-bold text-center text-primary font-caveat tracking-widest text-xl md:text-2xl lg:text-3xl mt-6"
             >
                 {x.isIT ? "Hey, io sono" : "Hey, I’m"}
             </motion.h1>
             <motion.h2
                 variants={headerItem}
-                className="font-bold text-center font-caveat text-primary mt-2 text-6xl sm:text-7xl md:text-8xl lg:text-9xl"
+                className="drop-shadow-lg font-bold text-center font-caveat mt-2 text-6xl sm:text-7xl md:text-8xl lg:text-9xl"
             >
                 Federico Gentili
             </motion.h2>
             <motion.h3
                 variants={headerItem}
-                className="font-bold text-center font-caveat mt-1 mb-1 text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-foreground-500"
+                className="drop-shadow-lg font-bold text-center font-caveat mt-1 mb-1 text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-foreground-500"
             >
                 {x.isIT
                     ? "Benvenuto nel mio spazio digitale"
@@ -55,11 +55,25 @@ export default function HomeHeader(x) {
             </motion.h3>
             <motion.p
                 variants={headerItem}
-                className="text-center text-lg sm:text-xl md:text-2xl lg:text-3xl font-caveat mt-5 text-foreground-500"
+                className="drop-shadow-lg text-center text-xl md:text-2xl lg:text-3xl font-caveat mt-5 text-foreground-500"
             >
-                {x.isIT
-                    ? "Sono uno sviluppatore web fullstack, proveniente da Ferrara, Italia."
-                    : "I'm an italian fullstack web developer, based near Ferrara, Italy."}
+                {x.isIT ? (
+                    <>
+                        Sono uno{" "}
+                        <span className="text-primary font-bold">
+                            sviluppatore web fullstack
+                        </span>
+                        , proveniente da Ferrara, Italia.
+                    </>
+                ) : (
+                    <>
+                        I'm an italian{" "}
+                        <span className="text-primary font-bold">
+                            fullstack web developer
+                        </span>
+                        , based near Ferrara, Italy.
+                    </>
+                )}
             </motion.p>
             <motion.div variants={headerItem}>
                 <Image
