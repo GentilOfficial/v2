be56f0ed753825c4c6709338b14c745b763c0fc7
GentilOfficial <federicogentili05@gmail.com>
2023-09-17 20:32:44 +0200

    changing font

--- pages/404.js
+++ pages/404.js
@@ -3,6 +3,9 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { Button } from "@nextui-org/react";
 import content from "@/locale/common";
+import { Caveat } from "next/font/google";
+
+const caveat = Caveat({ subsets: ["latin"] });
 
 export default function Error404() {
     const { locale, defaultLocale } = useRouter();
@@ -15,7 +18,9 @@ export default function Error404() {
             <main className="min-h-screen flex flex-col items-center justify-center text-center p-5">
                 <h1 className="text-xl font-medium text-primary mt-6">{h1}</h1>
                 <h2 className="mt-3 text-4xl font-semibold">{h2}</h2>
-                <p className="mt-2 text-xl text-foreground/80 font-caveat">
+                <p
+                    className={`mt-2 text-xl text-foreground/80 ${caveat.className}`}
+                >
                     {p}
                 </p>
                 <Button
