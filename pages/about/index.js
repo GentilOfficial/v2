1ef960b05c060297a65fc8e70988ccc3be37e59e
GentilOfficial <federicogentili05@gmail.com>
2023-10-10 10:27:30 +0200

    added titles

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { Divider, Image } from "@nextui-org/react";
 import { translate } from "@/data/site.config";
 import SEO from "@/components/SEO";
 
@@ -13,7 +14,13 @@ export default function AboutPage() {
             <section
                 className={`flex h-full flex-col items-center justify-center`}
             >
-                <h1>{title}</h1>
+                <div className="grid grid-cols-2 w-full">
+                    <Image src="https://nextui-docs-v2.vercel.app/images/album-cover.png" />
+                    <div>
+                        <h1 className="text-2xl">{title}</h1>
+                        <Divider />
+                    </div>
+                </div>
             </section>
         </>
     );
--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,8 +1,11 @@
+import { Caveat } from "next/font/google";
 import { useRouter } from "next/router";
 import { Divider, Progress } from "@nextui-org/react";
 import { FaStar, FaGear, FaUser } from "react-icons/fa6";
 import { translate, skillsList } from "@/data/site.config";
 import SEO from "@/components/SEO";
+
+const caveat = Caveat({ subsets: ["latin"] });
 
 export default function SkillsPage() {
     const { locale } = useRouter();
@@ -13,55 +16,65 @@ export default function SkillsPage() {
         <>
             <SEO title={title} description={description} />
             <section
-                className={`flex h-full flex-col items-center justify-center text-center gap-12 mb-36`}
+                className={`flex h-full flex-col items-center justify-center text-center mb-36`}
             >
-                <div className="flex flex-col gap-8 w-full">
-                    <h1 className="uppercase font-bold text-2xl flex items-center justify-center gap-2">
-                        {skillsList.top.title[locale]}{" "}
-                        <FaStar className="text-amber-400" />
-                    </h1>
-                    <div className="grid grid-cols-3 gap-8">
-                        {skillsList.top.skills.map((skill, index) => (
-                            <div
-                                key={`${skill}-${index}`}
-                                className="text-center flex flex-col items-center justify-center"
-                            >
-                                <skill.logo className="w-1/2" />
-                                <p>{skill.title}</p>
-                            </div>
-                        ))}
+                <h1
+                    className={`text-6xl text-primary font-bold ${caveat.className}`}
+                >
+                    {title}
+                </h1>
+                <div className="flex flex-col gap-12 h-full w-full items-center justify-start md:justify-center mt-12 md:mt-0">
+                    <Divider />
+                    <div className="flex flex-col gap-8 w-full">
+                        <h2 className="uppercase font-bold text-2xl flex items-center justify-center gap-2">
+                            {skillsList.top.title[locale]}{" "}
+                            <FaStar className="text-amber-400" />
+                        </h2>
+                        <div className="grid grid-cols-3 gap-8">
+                            {skillsList.top.skills.map((skill, index) => (
+                                <div
+                                    key={`${skill}-${index}`}
+                                    className="text-center flex flex-col items-center justify-center"
+                                >
+                                    <skill.logo className="w-1/2" />
+                                    <p>{skill.title}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-                <Divider />
-                <div className="flex flex-col gap-8 w-full">
-                    <h2 className="uppercase font-bold text-2xl flex items-center justify-center gap-2">
-                        {skillsList.technical.title[locale]} <FaGear />
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                        {skillsList.technical.skills.map((skill, index) => (
-                            <Progress
-                                key={`${skill}-${index}`}
-                                label={skill.title}
-                                value={skill.percentage}
-                                showValueLabel
-                            />
-                        ))}
+                    <Divider />
+                    <div className="flex flex-col gap-8 w-full">
+                        <h2 className="uppercase font-bold text-2xl flex items-center justify-center gap-2">
+                            {skillsList.technical.title[locale]} <FaGear />
+                        </h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                            {skillsList.technical.skills.map((skill, index) => (
+                                <Progress
+                                    key={`${skill}-${index}`}
+                                    label={skill.title}
+                                    value={skill.percentage}
+                                    showValueLabel
+                                />
+                            ))}
+                        </div>
                     </div>
-                </div>
-                <Divider />
-                <div className="flex flex-col gap-8 w-full">
-                    <h2 className="uppercase font-bold text-2xl flex items-center justify-center gap-2">
-                        {skillsList.professional.title[locale]} <FaUser />
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                        {skillsList.professional.skills.map((skill, index) => (
-                            <Progress
-                                key={`${skill}-${index}`}
-                                label={skill.title[locale]}
-                                value={skill.percentage}
-                                showValueLabel
-                            />
-                        ))}
+                    <Divider />
+                    <div className="flex flex-col gap-8 w-full">
+                        <h2 className="uppercase font-bold text-2xl flex items-center justify-center gap-2">
+                            {skillsList.professional.title[locale]} <FaUser />
+                        </h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                            {skillsList.professional.skills.map(
+                                (skill, index) => (
+                                    <Progress
+                                        key={`${skill}-${index}`}
+                                        label={skill.title[locale]}
+                                        value={skill.percentage}
+                                        showValueLabel
+                                    />
+                                )
+                            )}
+                        </div>
                     </div>
                 </div>
             </section>
