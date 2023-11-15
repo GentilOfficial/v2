119018b982353384264bd82daf4b45ad38b4af6c
GentilOfficial <federicogentili05@gmail.com>
2023-11-16 00:49:09 +0100

    beginning of the about page identification

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,7 +9,8 @@ const caveat = Caveat({ subsets: ["latin"] });
 export default function AboutPage() {
     const { locale } = useRouter();
 
-    const { title, description } = translate.about[locale];
+    const { title, description, background, experiences } =
+        translate.about[locale];
 
     return (
         <>
@@ -25,28 +26,19 @@ export default function AboutPage() {
                         <Image src="https://images.unsplash.com/photo-1600793575654-910699b5e4d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" />
                     </div>
                     <div className="flex flex-col w-1/2 gap-4">
-                        <h2 className="text-2xl">{title}</h2>
+                        <h2 className="text-2xl">{background.title}</h2>
                         <Divider />
-                        <p>
-                            "Sed ut perspiciatis unde omnis iste natus error sit
-                            voluptatem accusantium doloremque laudantium, totam
-                            rem aperiam, eaque ipsa quae ab illo inventore
-                            veritatis et quasi architecto beatae vitae dicta
-                            sunt explicabo. Nemo enim ipsam voluptatem quia
-                            voluptas sit aspernatur aut odit aut fugit, sed quia
-                            consequuntur magni dolores eos qui ratione
-                            voluptatem sequi nesciunt. Neque porro quisquam est,
-                            qui dolorem ipsum quia dolor sit amet, consectetur,
-                            adipisci velit, sed quia non numquam eius modi
-                            tempora incidunt ut labore et dolore magnam aliquam
-                            quaerat voluptatem. Ut enim ad minima veniam, quis
-                            nostrum exercitationem ullam corporis suscipit
-                            laboriosam, nisi ut aliquid ex ea commodi
-                            consequatur? Quis autem vel eum iure reprehenderit
-                            qui in ea voluptate velit esse quam nihil molestiae
-                            consequatur, vel illum qui dolorem eum fugiat quo
-                            voluptas nulla pariatur?"
-                        </p>
+                        <p>{background.p}</p>
+                        <h2 className="text-2xl">{experiences.title}</h2>
+                        <Divider />
+                        {experiences.list.map((item, index) => (
+                            <div key={`${item}-${index}`}>
+                                <p>{item.title}</p>
+                                <p>{item.description}</p>
+                                <p>{item.start}</p>
+                                <p>{item.end}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
